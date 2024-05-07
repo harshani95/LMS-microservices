7a373bf8774bb052e3911eec5e678691bc5010cb
harshani <ruwanthikapemarathna@gmail.com>
2024-05-07 18:07:09 +0530

    added gateway service

---
+++ gateway-service-api/src/main/java/com/devstack/lms/gatewayserviceapi/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.devstack.lms.gatewayserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
---
+++ gateway-service-api/src/test/java/com/devstack/lms/gatewayserviceapi/GatewayServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.lms.gatewayserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
